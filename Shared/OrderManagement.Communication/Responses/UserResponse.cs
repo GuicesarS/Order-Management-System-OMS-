74436cd9d0a25ebbc85c27e0e6a6821823d03087
guicesars <guicesarsosil@gmail.com>
2026-01-06 17:04:43 -0300

    feat(shared): add user DTOs and responses

---
+++ Shared/OrderManagement.Communication/Responses/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderManagement.Communication.Responses;
+public class UserResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
