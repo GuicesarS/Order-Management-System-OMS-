5e128520f26a4df105b210029e0429577af658c7
guicesars <guicesarsosil@gmail.com>
2026-01-06 17:03:34 -0300

    feat(domain): add Email value object and update User entity

--- Source/OrderManagement.Domain/Entities/User/User.cs
+++ Source/OrderManagement.Domain/Entities/User/User.cs
@@ -1,18 +1,20 @@
-﻿namespace OrderManagement.Domain.Entities.User;
+﻿using OrderManagement.Domain.ValueObjects;
+
+namespace OrderManagement.Domain.Entities.User;
 
 public class User
 {
     public Guid Id { get; private set; }
     public string Name { get; private set; }
-    public string Email { get; private set; }
-    public DateTime CreatedAt{ get; set; }
+    public Email Email { get; private set; }
+    public DateTime CreatedAt{ get; private set; }
 
     protected User() { } // Ef Core
 
-    public User(string nome, string email)
+    public User(string name, Email email)
     {
         Id = Guid.NewGuid();
-        Name = nome;
+        Name = name;
         Email = email;
         CreatedAt = DateTime.UtcNow;
     }
---
+++ Source/OrderManagement.Domain/ValueObjects/Email.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mail;
+
+namespace OrderManagement.Domain.ValueObjects;
+
+public sealed class Email
+{
+    public string Value { get; }
+
+    private Email(string value)
+    {
+        Value = value;
+    }
+
+    public static Email Create(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email is required.");
+        // add DomainException later
+
+        try
+        {
+            var mailAddress = new MailAddress(email);
+
+            return new Email(mailAddress.Address.ToLowerInvariant());
+        }
+        catch (FormatException)
+        {
+            throw new ArgumentException("Email format is invalid.");
+            // add DomainException later
+        }
+    }
+
+    public override string ToString() => Value;
+}
