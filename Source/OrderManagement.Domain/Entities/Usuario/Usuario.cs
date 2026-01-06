9de305590236721b825e709d456db442a9889987
guicesars <guicesarsosil@gmail.com>
2026-01-06 16:10:04 -0300

    feat(domain): add user entity

---
+++ Source/OrderManagement.Domain/Entities/Usuario/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderManagement.Domain.Entities.Usuario;
+
+public class Usuario
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public DateTime Criacao { get; set; }
+
+    protected Usuario() { } // Ef Core
+
+    public Usuario(string nome, string email)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        Criacao = DateTime.UtcNow;
+    }
+}
