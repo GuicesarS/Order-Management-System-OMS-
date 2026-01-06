535f66d0c72bd5c7bb3461b59453ea82ffaeb47e
guicesars <guicesarsosil@gmail.com>
2026-01-06 16:19:11 -0300

    chore: rename projects and files to English

---
+++ Source/OrderManagement.Domain/Entities/User/User.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderManagement.Domain.Entities.User;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public DateTime CreatedAt{ get; set; }
+
+    protected User() { } // Ef Core
+
+    public User(string nome, string email)
+    {
+        Id = Guid.NewGuid();
+        Name = nome;
+        Email = email;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
