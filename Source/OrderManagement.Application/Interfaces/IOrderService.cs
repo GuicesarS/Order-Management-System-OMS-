d93a1303fe966e4acffe0f4b6227ee5e10720c1f
guicesars <guicesarsosil@gmail.com>
2026-01-12 16:28:03 -0300

    feat(application): add IOrderService contract

---
+++ Source/OrderManagement.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using OrderManagement.Application.Common;
+using OrderManagement.Communication.Dtos.Order;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.Interfaces;
+
+public interface IOrderService
+{
+    Result<OrderResponse> Create(CreateOrderDto createOrderDto);
+    Result<OrderResponse> Update(UpdateOrderDto updateOrderDto);
+    Result<OrderResponse> GetById(Guid id);
+    Result<IEnumerable<OrderResponse>> GetAll();
+    Result<bool> Delete(Guid id);
+}
