7c03b4a023cc58300b88db1429f012b8f6317cbd
guicesars <guicesarsosil@gmail.com>
2026-01-12 16:11:02 -0300

    feat(application): add IUserService contract

---
+++ Source/OrderManagement.Application/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using OrderManagement.Application.Common;
+using OrderManagement.Communication.Dtos.User;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.Interfaces;
+
+public interface IUserService
+{
+    Result<UserResponse> Create(CreateUserDto createUserDto);
+    Result<UserResponse> Update(Guid id, UpdateUserDto updateUserDto);
+    Result<UserResponse> GetById(Guid id);
+    Result<IEnumerable<UserResponse>> GetAll();
+    Result<bool> Delete(Guid id);
+}
