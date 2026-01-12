3bda510729e9e8d0087c2f47fd7d5320ba09defd
guicesars <guicesarsosil@gmail.com>
2026-01-12 15:32:22 -0300

    feat(domain): add IProductRepository contract

---
+++ Source/OrderManagement.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using OrderManagement.Domain.Entities.Product;
+
+namespace OrderManagement.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Product? GetProductById(Guid productId);
+    IEnumerable<Product> GetAll();
+    void Add(Product product);
+    void Update(Product product);
+    void Delete(Guid id);
+}
