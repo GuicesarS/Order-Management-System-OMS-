ed3cfeffc460c666fdb27bf80da8543359af2d59
guicesars <guicesarsosil@gmail.com>
2026-01-12 16:06:57 -0300

    feat(application): add generic Result<T> wrapper for service responses

---
+++ Source/OrderManagement.Application/Common/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderManagement.Application.Common;
+
+public class Result<T>
+{
+    public bool Success { get; }
+    public string ErrorMessage { get; }
+    public T? Data { get; }
+
+    public Result(bool success, string errorMessage, T? data)
+    {
+        Success = success;
+        Data = data;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result<T> Ok(T data) => new Result<T>(true, string.Empty, data);
+    public static Result<T> Failure(string errorMessage) => new Result<T>(false, errorMessage, default);
+}
