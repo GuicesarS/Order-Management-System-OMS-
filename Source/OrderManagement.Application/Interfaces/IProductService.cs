4e3c4f23db9f9e3739a6f51f122c5597fb25d238
guicesars <guicesarsosil@gmail.com>
2026-01-12 16:20:49 -0300

    feat(application): add IProductService contract

---
+++ Source/OrderManagement.Application/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using OrderManagement.Application.Common;
+using OrderManagement.Communication.Dtos.Product;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.Interfaces;
+
+public interface IProductService
+{
+    Result<ProductResponse> Create(CreateProductDto createProductDto);
+    Result<ProductResponse> Update(UpdateProductDto updateProductDto);
+    Result<ProductResponse> GetById(Guid id);
+    Result<IEnumerable<ProductResponse>> GetAll();
+    Result<bool> Delete(Guid id);
+}
