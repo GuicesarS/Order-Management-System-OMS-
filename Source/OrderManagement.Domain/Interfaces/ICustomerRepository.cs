a90544a014109bed04b5131e3df2c88fc18540e3
guicesars <guicesarsosil@gmail.com>
2026-01-12 15:30:35 -0300

    feat(domain): add ICustomerRepository contract

---
+++ Source/OrderManagement.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using OrderManagement.Domain.Entities.Customer;
+
+namespace OrderManagement.Domain.Interfaces;
+
+public interface ICustomerRepository
+{
+    Customer? GetCustomerById(Guid customerId);
+    IEnumerable<Customer> GetAll();
+    void Add(Customer customer);
+    void Update(Customer customer);
+    void Delete(Guid id);
+}
