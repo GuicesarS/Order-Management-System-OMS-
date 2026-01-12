f6ff6ab4e9b29340ad6a517e392067bf6a256367
guicesars <guicesarsosil@gmail.com>
2026-01-12 15:27:20 -0300

    feat(domain): add IUserRepository contract

---
+++ Source/OrderManagement.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using OrderManagement.Domain.Entities.User;
+
+namespace OrderManagement.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    User? GetOrderById(Guid userId);
+    IEnumerable<User> GetAll();
+    void Add(User order);
+    void Update(User order);
+    void Delete(Guid id);
+}
