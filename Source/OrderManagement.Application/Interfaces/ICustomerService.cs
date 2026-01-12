bea9c0d37bc6bcacc7bfc2540ca3a56efb4c051b
guicesars <guicesarsosil@gmail.com>
2026-01-12 16:18:09 -0300

    feat(application): add ICustomerService contract

---
+++ Source/OrderManagement.Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using OrderManagement.Application.Common;
+using OrderManagement.Communication.Dtos.Customer;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.Interfaces;
+
+public interface ICustomerService
+{
+    Result<CustomerResponse> Create(CreateCustomerDto createCustomerDto);
+    Result<CustomerResponse> Update(UpdateCustomerDto updateCustomerDto);
+    Result<CustomerResponse> GetById(Guid id);
+    Result<IEnumerable<CustomerResponse>> GetAll();
+    Result<bool> Delete(Guid id);
+}
