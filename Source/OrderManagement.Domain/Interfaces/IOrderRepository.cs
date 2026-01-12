6b47757da787c99e8ab9cf78185f9e6b112963f7
guicesars <guicesarsosil@gmail.com>
2026-01-12 15:33:53 -0300

    feat(domain): add IOrderRepository contract

---
+++ Source/OrderManagement.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using OrderManagement.Domain.Entities.Order;
+
+namespace OrderManagement.Domain.Interfaces;
+
+public interface IOrderRepository
+{
+    Order? GetOrderById(Guid orderId);
+    IEnumerable<Order> GetAll();
+    void Add(Order order);
+    void Update(Order order);
+    void Delete(Guid id);
+}
