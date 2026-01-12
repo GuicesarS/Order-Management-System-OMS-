4c5cbbc238de89181ef06b76943324f5c48f3b25
guicesars <guicesarsosil@gmail.com>
2026-01-12 14:30:28 -0300

    feat(communication): add customer Dtos

---
+++ Shared/OrderManagement.Communication/Dtos/Customer/UpdateCustomerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderManagement.Communication.Dtos.Customer;
+
+public class UpdateCustomerDto
+{
+    public string? Name { get; set; } 
+    public string? Email { get; set; } 
+    public string? Phone { get; set; } 
+    public string? Address { get; set; } 
+}
