8dc2237aaaee51d1709b77abea094327c9456f7f
guicesars <guicesarsosil@gmail.com>
2026-01-13 14:37:00 -0300

    feat(application): add customer validator dto

---
+++ Source/OrderManagement.Application/Validators/Customer/CreateCustomerDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Dtos.Customer;
+
+namespace OrderManagement.Application.Validators.Customer;
+
+public class CreateCustomerDtoValidator : AbstractValidator<CreateCustomerDto>
+{
+    public CreateCustomerDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+    }
+
+}
---
+++ Source/OrderManagement.Application/Validators/Customer/UpdateCustomerDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Dtos.Customer;
+
+namespace OrderManagement.Application.Validators.Customer;
+
+public class UpdateCustomerDtoValidator : AbstractValidator<UpdateCustomerDto>
+{
+    public UpdateCustomerDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Name));
+
+        RuleFor(x => x.Email)
+            .EmailAddress().WithMessage("Invalid email format.")
+            .When(x => !string.IsNullOrEmpty(x.Email));
+    }
+
+}
