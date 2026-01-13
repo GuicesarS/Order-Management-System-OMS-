446bbcdd578caf825f2de31b602cc6a8d6f3b745
guicesars <guicesarsosil@gmail.com>
2026-01-13 14:32:59 -0300

    feat(application): add order validators dto

---
+++ Source/OrderManagement.Application/Validators/Order/CreateOrderDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Dtos.Order;
+
+namespace OrderManagement.Application.Validators.Order;
+
+public class CreateOrderDtoValidator : AbstractValidator<CreateOrderDto>
+{
+    public CreateOrderDtoValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty().WithMessage("CustomerId is required.");
+
+        RuleFor(x => x.Items)
+            .NotEmpty().WithMessage("Order must contain at least one item.");
+    }
+
+}
---
+++ Source/OrderManagement.Application/Validators/Order/UpdateOrderDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Dtos.Order;
+
+namespace OrderManagement.Application.Validators.Order;
+
+
+public class UpdateOrderDtoValidator : AbstractValidator<UpdateOrderDto>
+{
+    public UpdateOrderDtoValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty().WithMessage("CustomerId cannot be empty.")
+            .When(x => x.CustomerId.HasValue);
+
+        RuleFor(x => x.Status)
+            .MaximumLength(50).WithMessage("Status cannot exceed 50 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Status));
+    }
+
+}
