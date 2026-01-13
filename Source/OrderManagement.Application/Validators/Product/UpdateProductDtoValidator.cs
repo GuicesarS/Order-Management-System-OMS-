7292fe721819b1a5ce87d2e442622c5e35900c8e
guicesars <guicesarsosil@gmail.com>
2026-01-13 14:39:29 -0300

    feat(application): add product validator dto

---
+++ Source/OrderManagement.Application/Validators/Product/UpdateProductDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Dtos.Product;
+
+namespace OrderManagement.Application.Validators.Product;
+
+public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
+{
+    public UpdateProductDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(100).WithMessage("Product name cannot exceed 100 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Name));
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero.")
+            .When(x => x.Price.HasValue);
+
+        RuleFor(x => x.StockQuantity)
+            .GreaterThanOrEqualTo(0).WithMessage("Stock quantity cannot be negative.")
+            .When(x => x.StockQuantity.HasValue);
+
+    }
+}
