d9245b1668844539aba9999e97d64b69e94cf9d5
guicesars <guicesarsosil@gmail.com>
2026-01-15 14:13:33 -0300

    feat(domain,infra): add async ICustomerRepository and implement CustomerRepository with EF Core

--- Source/OrderManagement.Domain/Interfaces/ICustomerRepository.cs
+++ Source/OrderManagement.Domain/Interfaces/ICustomerRepository.cs
@@ -4,9 +4,10 @@ namespace OrderManagement.Domain.Interfaces;
 
 public interface ICustomerRepository
 {
-    Customer? GetCustomerById(Guid customerId);
-    IEnumerable<Customer> GetAll();
-    void Add(Customer customer);
-    void Update(Customer customer);
-    void Delete(Guid id);
+    Task<Customer?> GetCustomerById(Guid customerId);
+    Task<IEnumerable<Customer>> GetAllAsync();
+    Task<Customer?> GetCustomerByEmail(string email);
+    Task AddAsync(Customer customer);
+    Task UpdateAsync(Customer customer);
+    Task DeleteAsync(Guid id);
 }
---
+++ Source/OrderManagement.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities.Customer;
+using OrderManagement.Domain.Interfaces;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly OrderManagementDbContext _dbContext;
+    public CustomerRepository(OrderManagementDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddAsync(Customer customer)
+    {
+        await _dbContext.Customers.AddAsync(customer);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var customer = await _dbContext.Customers.FindAsync(id);
+        if(customer != null)
+        {
+            _dbContext.Customers.Remove(customer);
+            await _dbContext.SaveChangesAsync();
+        }
+        
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+       return await _dbContext.Customers.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Customer?> GetCustomerByEmail(string email)
+    {
+        return await _dbContext.Customers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Email.Value == email);
+    }
+
+    public async Task<Customer?> GetCustomerById(Guid customerId)
+    {
+        return await _dbContext.Customers
+             .AsNoTracking()
+             .FirstOrDefaultAsync(c => c.Id == customerId);
+    }
+
+    public async Task UpdateAsync(Customer customer)
+    {
+        _dbContext.Customers.Update(customer);
+        await _dbContext.SaveChangesAsync();
+
+    }
+}
