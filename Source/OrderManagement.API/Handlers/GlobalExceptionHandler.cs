b7fc246d4c106a5b57f0a52f3bd3e5d0892cfb5d
guicesars <guicesarsosil@gmail.com>
2026-01-16 18:34:38 -0300

    feat(exceptions): add domain and application custom exceptions with global handler support

---
+++ Source/OrderManagement.API/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Application.Exceptions;
+using OrderManagement.Domain.Exception;
+
+namespace OrderManagement.API.Handlers;
+
+internal sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "An unhandled exception occurred: {Message}.", exception.Message);
+
+        var statusCode = exception switch
+        {
+            ValidationException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            DomainValidationException => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = "An error occured.",
+            Detail = exception.InnerException?.Message ?? exception.Message
+           
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
---
+++ Source/OrderManagement.Domain/Exception/DomainValidationException.cs
@@ -0,0 +1,8 @@
+﻿
+using System;
+namespace OrderManagement.Domain.Exception;
+
+public class DomainValidationException : System.Exception
+{
+    public DomainValidationException(string? message) : base(message) { }
+}
