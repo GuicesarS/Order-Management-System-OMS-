a8ff8ef5d1fff3107e88112acbeaf75af957243f
guicesars <guicesarsosil@gmail.com>
2026-01-19 17:01:31 -0300

    feat(application): add reusable extension to normalize update values

---
+++ Source/OrderManagement.Application/Common/Extensions/UpdateValueExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagement.Application.Common.Extensions;
+
+public static class UpdateValueExtension
+{
+    public static string? GetValueForUpdate(this string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || value.Equals("string", StringComparison.OrdinalIgnoreCase))
+            return null;
+
+        return value;
+    }
+}
