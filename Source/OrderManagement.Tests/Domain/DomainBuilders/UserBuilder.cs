ac7840accfd1dff53abb987168628f8281baf2d4
guicesars <guicesarsosil@gmail.com>
2026-01-20 17:13:51 -0300

    test(user): add unit tests for User entity constructor and update methods

---
+++ Source/OrderManagement.Tests/Domain/DomainBuilders/UserBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using OrderManagement.Domain.Entities.User;
+using OrderManagement.Domain.Enums;
+using OrderManagement.Domain.ValueObjects;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OrderManagement.Tests.Domain.DomainBuilders;
+
+public class UserBuilder
+{
+    private readonly Faker<User> _faker;
+    public UserBuilder()
+    {
+        _faker = new Faker<User>()
+             .CustomInstantiator(f => 
+             new User(f.Person.FirstName,Email.Create(f.Internet.Email()), f.PickRandom<UserRole>()
+             ));
+
+    }
+
+    public User Build() => _faker.Generate();
+    public User BuildInvalidUserWithEmptyName() => 
+        new User("", Email.Create("teste@gmail.com"), UserRole.Admin);
+    public User BuildInvalidUserWithNullEmail()
+        => new User("Guilherme", null!, UserRole.Admin);
+    public User BuildInvalidUserWithInvalidRole()
+        => new User("Guilherme", Email.Create("teste@email.com"), (UserRole)999);
+
+
+
+}
---
+++ Source/OrderManagement.Tests/Domain/Entities/UserTest.cs
@@ -0,0 +1,188 @@
+using FluentAssertions;
+using OrderManagement.Domain.Enums;
+using OrderManagement.Domain.Exception;
+using OrderManagement.Domain.ValueObjects;
+using OrderManagement.Tests.Domain.DomainBuilders;
+
+namespace OrderManagement.Tests.Domain.Entities;
+public class UserTest
+{
+    // Constructor Tests
+
+    [Fact]
+    public void Constructor_ShouldCreateUser_WhenValidData()
+    {
+        var user = new UserBuilder().Build();
+
+        user.Id.Should().NotBeEmpty();
+        user.Name.Should().NotBeNullOrWhiteSpace();
+        user.Email.Should().NotBeNull();
+        Enum.IsDefined(typeof(UserRole), user.Role).Should().BeTrue();
+
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenNameIsEmpty()
+    {
+        var user = new UserBuilder();
+
+        Action act = () => user.BuildInvalidUserWithEmptyName();
+
+        act.Should().
+            Throw<DomainValidationException>()
+           .WithMessage("Name cannot be empty.");
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenEmailIsNull()
+    {
+        var user = new UserBuilder();
+
+        Action act = () => user.BuildInvalidUserWithNullEmail();
+
+        act.Should()
+            .Throw<DomainValidationException>()
+            .WithMessage("Email cannot be null.");
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenRoleIsInvalid()
+    {
+        var user = new UserBuilder();
+
+        Action act = () => user.BuildInvalidUserWithInvalidRole();
+
+        act.Should()
+            .Throw<DomainValidationException>();
+    }
+
+    //Update Name Test
+
+    [Fact]
+    public void UpdateName_ShouldBeSuccessful_WhenValidData()
+    {
+        var user = new UserBuilder().Build();
+
+        var updateName = "New name";
+
+        user.UpdateName(updateName);
+
+        user.Should().NotBeNull();
+        user.Name.Should().Be(updateName);
+    }
+
+    [Fact]
+    public void UpdateName_ShouldThrowException_WhenNameIsEmpty()
+    {
+        var user = new UserBuilder().Build();
+
+        Action act = () => user.UpdateName("");
+
+        act.Should()
+       .Throw<DomainValidationException>()
+       .WithMessage("Name cannot be empty.");
+    }
+
+    // Update Email Test
+
+    [Fact]
+    public void UpdateEmail_ShouldBeSuccessful_WhenValidData()
+    {
+        var user = new UserBuilder().Build();
+
+        var newEmail = Email.Create("newemail@gmail.com");
+
+        user.UpdateEmail(newEmail);
+
+        user.Should().NotBeNull();
+        user.Email.Should().Be(newEmail);
+        user.Email.Value.Should().Be("newemail@gmail.com");
+    }
+
+    [Fact]
+    public void UpdateEmail_ShouldThrowException_WhenEmailIsInvalid()
+    {
+        var user = new UserBuilder().Build();
+
+        Action act = () => user.UpdateEmail(Email.Create("invalid-email"));
+
+        act.Should().Throw<DomainValidationException>()
+            .WithMessage("Email format is invalid.");
+    }
+
+    [Fact]
+    public void UpdateEmail_ShouldThrowException_WhenEmailIsNull()
+    {
+        var user = new UserBuilder().Build();
+
+        Action act = () => user.UpdateEmail(Email.Create(null!));
+
+        act.Should().Throw<DomainValidationException>()
+            .WithMessage("Email is required.");
+    }
+
+    // Update Role Test
+
+    [Fact]
+    public void UpdateRole_ShouldBeSuccessful_WhenValidData()
+    {
+        var user = new UserBuilder().Build();
+        var newRole = UserRole.Operator;
+
+        user.UpdateRole(newRole);
+
+        user.Role.Should().Be(newRole);
+    }
+
+    [Fact]
+    public void UpdateRole_ShouldThrowException_WhenRoleIsInvalid()
+    {
+        var user = new UserBuilder().Build();
+
+        Action act = () => user.UpdateRole((UserRole)999);
+
+        act.Should().Throw<DomainValidationException>();
+        
+    }
+
+    // Update User Test
+
+    [Fact]
+    public void UpdateUser_ShouldBeSuccessful_WhenValidData()
+    {
+        var user = new UserBuilder().Build();
+
+        var newName = "João Pereira";
+        var newEmail = Email.Create("joao@gmail.com");
+        var newRole = UserRole.Operator;
+
+        user.UpdateUser(newName, newEmail, newRole);
+
+        user.Should().NotBeNull();
+        user.Name.Should().Be(newName);
+    }
+
+    [Theory]
+    [InlineData("", "teste@gmail.com", UserRole.Admin, "Name cannot be empty.")]
+    [InlineData("Guilherme", null, UserRole.Admin, "email")]
+    [InlineData("Guilherme", "teste@gmail.com", (UserRole)999, "not valid")]
+    public void UpdateUser_ShouldThrowException_WhenDataIsInvalid(
+     string name,
+     string? emailValue,
+     UserRole role,
+     string expectedMessage
+ )
+    {
+        var user = new UserBuilder().Build();
+
+        Email? email = emailValue is null 
+            ? null
+            : Email.Create(emailValue);
+
+        Action act = () => user.UpdateUser(name, email!, role);
+
+        act.Should()
+            .Throw<DomainValidationException>()
+            .WithMessage($"*{expectedMessage}*");
+    }
+}
