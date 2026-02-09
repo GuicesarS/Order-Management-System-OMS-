3e24d290c8601e94f1f4044ebd10c47ee684e463
Guilherme Soares <guicesarsosil@gmail.com>
2026-02-09 20:34:07 -0300

    feat(auth): add JWT dependency injection setup

--- Source/OrderManagement.Infrastructure/InfrastructureDependencyInjection.cs
+++ Source/OrderManagement.Infrastructure/InfrastructureDependencyInjection.cs
@@ -1,18 +1,33 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using OrderManagement.Domain.Interfaces;
+using OrderManagement.Domain.Security.Token;
 using OrderManagement.Infrastructure.Repositories;
+using OrderManagement.Infrastructure.Security.Token;
 
 namespace OrderManagement.Infrastructure;
 
 public static class InfrastructureDependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();  
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IOrderRepository, OrderRepository>();
 
+        AddTokens(services, configuration);
+
         return services;
+    }
+
+    private static void AddTokens(IServiceCollection services, IConfiguration configuration)
+    {
+        var expirationTimeInMinutes = configuration.GetValue<int>("Settings:Jwt:ExpirationTimeMinutes");
+
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+        services.AddScoped<IAccessTokenGenerator>(provider =>
+            new AccessTokenGenerator(expirationTimeInMinutes, signingKey));
     }
 }
